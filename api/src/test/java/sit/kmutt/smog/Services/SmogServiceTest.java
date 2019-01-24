22d4f2f2e514876bde1bdf297b8bc4984d924466
Keerati <keerati.jear@gmail.com>
2019-01-24 12:27:40 +0700

    :white_check_mark: add service test file

---
+++ api/src/test/java/sit/kmutt/smog/Services/SmogServiceTest.java
@@ -0,0 +1,12 @@
+package sit.kmutt.smog.Services;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SmogServiceTest {
+
+    @Test
+    public void testGetRecentSmog() {
+        Assert.assertEquals(null, null);
+    }
+}
