35eccf91d62630a2d4a92a926b2e18840aca231c
Keerati <keerati.jear@gmail.com>
2019-01-24 09:36:20 +0700

    :tada: init project

---
+++ api/src/main/java/sit/kmutt/smog/SmogApplication.java
@@ -0,0 +1,14 @@
+package sit.kmutt.smog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmogApplication.class, args);
+	}
+
+}
+
